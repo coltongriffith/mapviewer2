459ae85f147241c937480e8b353fcaff30228ab1
Colton Griffith <109363699+coltongriffith@users.noreply.github.com>
2026-03-27 10:25:34 -0700

    Create NorthArrow.jsx

---
+++ src/components/NorthArrow.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NorthArrow() {
+  return (
+    <div className="north-arrow">
+      <div className="north-arrow-label">N</div>
+      <div className="north-arrow-arrow">▲</div>
+    </div>
+  );
+}
